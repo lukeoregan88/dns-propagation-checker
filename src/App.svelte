1ca0bf9c68c78f774593a19b02ebc88ec28f8fbf
Direct Design <admin@dda.co.uk>
2024-07-30 11:55:05 +0100

    Add ReCAPTCHA and a background video

--- src/App.svelte
+++ src/App.svelte
@@ -1,14 +1,19 @@
 <!-- @format -->
 <script>
   import CobeGlobe from "./CobeGlobe.svelte";
+
   let domain = "";
   let recordType = "A";
   let expectedIp = "";
-  let results = [];
   let loading = false;
+  let showExpected = false;
+  let recaptchaToken = "";
   let error = null;
+  let results = [];
   let markers = [];
   let markersReady = false;
+
+  const isProduction = process.env.NODE_ENV === "production";
 
   const dnsServers = [
     { ip: "8.8.8.8", name: "Google DNS" },
@@ -48,6 +53,45 @@
     results = [];
     markers = [];
     markersReady = false;
+
+    if (isProduction) {
+      // Generate reCAPTCHA token
+      // @ts-ignore
+      grecaptcha.ready(async () => {
+        // @ts-ignore
+        recaptchaToken = await grecaptcha.execute(
+          "6LfJ1bsZAAAAAFZIfAjw0M8zVRnEopUxG67Bw7Sg",
+          {
+            action: "submit",
+          }
+        );
+
+        // Send the token to your PHP script for verification
+        const response = await fetch("verify-recaptcha.php", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+          },
+          body: new URLSearchParams({ token: recaptchaToken }),
+        });
+
+        const data = await response.json();
+        if (data.success) {
+          // Proceed with your existing checkPropagation logic
+          await performCheckPropagation();
+        } else {
+          console.error("reCAPTCHA verification failed", data.errors);
+          error = "reCAPTCHA verification failed";
+          loading = false;
+        }
+      });
+    } else {
+      // Skip reCAPTCHA in development
+      await performCheckPropagation();
+    }
+  }
+
+  async function performCheckPropagation() {
     try {
       const fetchPromises = dnsServers.map(async (server) => {
         const [dnsResult, location] = await Promise.all([
@@ -66,7 +110,7 @@
       });
       results = await Promise.all(fetchPromises);
       markersReady = true;
-      console.log("Markers:", markers);
+      //console.log("Markers:", markers);
     } catch (err) {
       error = err.message;
     } finally {
@@ -119,6 +163,16 @@
 </script>
 
 <main>
+  <div class="background-video">
+    <video
+      class="video"
+      autoplay
+      muted
+      loop
+      playsinline
+      src="./121470-724697516_small.mp4"
+    ></video>
+  </div>
   <div class="parent">
     <div class="div1">
       {#if markersReady}
@@ -144,11 +198,29 @@
           <option value="NS">NS</option>
           <option value="TXT">TXT</option>
         </select>
-        <input
-          type="text"
-          bind:value={expectedIp}
-          placeholder="Expected Result (optional)"
-        />
+        <button
+          class="expectedBtn"
+          type="button"
+          on:click={() => (showExpected = !showExpected)}
+          ><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"
+            ><path
+              class="fa-secondary"
+              opacity=".4"
+              d="M192 256a64 64 0 1 0 128 0 64 64 0 1 0 -128 0z"
+            /><path
+              class="fa-primary"
+              d="M489.6 191.2c6.9-6.2 9.6-15.9 6.4-24.6c-4.4-11.9-9.7-23.3-15.8-34.3l-4.7-8.1c-6.6-11-14-21.4-22.1-31.2c-5.9-7.2-15.7-9.6-24.5-6.8l-55.7 17.7C359.8 93.6 345 85 329.2 78.4L316.7 21.3c-2-9.1-9-16.3-18.2-17.8C284.7 1.2 270.5 0 256 0s-28.7 1.2-42.5 3.5c-9.2 1.5-16.2 8.7-18.2 17.8L182.8 78.4c-15.8 6.5-30.6 15.1-44 25.4L83.1 86.1c-8.8-2.8-18.6-.3-24.5 6.8c-8.1 9.8-15.5 20.2-22.1 31.2l-4.7 8.1c-6.1 11-11.4 22.4-15.8 34.3c-3.2 8.7-.5 18.4 6.4 24.6l43.3 39.4C64.6 238.9 64 247.4 64 256s.6 17.1 1.7 25.4L22.4 320.8c-6.9 6.2-9.6 15.9-6.4 24.6c4.4 11.9 9.7 23.3 15.8 34.3l4.7 8.1c6.6 11 14 21.4 22.1 31.2c5.9 7.2 15.7 9.6 24.5 6.8l55.7-17.7c13.4 10.3 28.2 18.9 44 25.4l12.5 57.1c2 9.1 9 16.3 18.2 17.8c13.8 2.3 28 3.5 42.5 3.5s28.7-1.2 42.5-3.5c9.2-1.5 16.2-8.7 18.2-17.8l12.5-57.1c15.8-6.5 30.6-15.1 44-25.4l55.7 17.7c8.8 2.8 18.6 .3 24.5-6.8c8.1-9.8 15.5-20.2 22.1-31.2l4.7-8.1c6.1-11 11.4-22.4 15.8-34.3c3.2-8.7 .5-18.4-6.4-24.6l-43.3-39.4c1.1-8.3 1.7-16.8 1.7-25.4s-.6-17.1-1.7-25.4l43.3-39.4zM256 160a96 96 0 1 1 0 192 96 96 0 1 1 0-192z"
+            /></svg
+          ></button
+        >
+        {#if showExpected}
+          <input
+            type="text"
+            bind:value={expectedIp}
+            placeholder="Expected Result (optional)"
+          />
+        {/if}
+
         <button type="submit" disabled={loading}>Check</button>
       </form>
 
@@ -214,117 +286,3 @@
     <strong>BTC address</strong>: 328fUT3qVNZJ8EbHGaZ3M1VvCzEbFGW39f
   </p>
 </footer>
-
-<style>
-  main {
-    min-height: 100vh;
-    width: 100vw;
-    height: auto;
-  }
-  footer {
-    text-align: center;
-    background-color: #ffffff;
-    color: #333;
-    width: calc(100vw - 2em);
-    padding: 1em;
-    height: calc(10vh - 2em);
-    display: flex;
-    align-items: center;
-    justify-content: center;
-  }
-  footer p {
-    margin: 0;
-  }
-  .parent {
-    display: grid;
-    grid-template-columns: repeat(2, 1fr);
-    grid-template-rows: 1fr;
-    grid-column-gap: 0px;
-    grid-row-gap: 0px;
-    width: 100vw;
-    min-height: 100vh;
-    height: auto;
-  }
-
-  .div1 {
-    grid-area: 1 / 1 / 2 / 2;
-    min-height: 100vh;
-    height: auto;
-    width: 50vw;
-  }
-  .div2 {
-    grid-area: 1 / 2 / 2 / 3;
-    min-height: calc(100vh - 2em);
-    height: auto;
-    padding: 2em 1em;
-    display: flex;
-    align-content: center;
-    align-items: stretch;
-    flex-direction: column;
-    justify-content: center;
-    width: calc(50vw - 2em);
-  }
-
-  input,
-  select {
-    padding: 0.5em;
-    margin-right: 0.5em;
-  }
-  button {
-    padding: 0.5em;
-  }
-
-  ul {
-    list-style: none;
-    margin: 0;
-    padding: 0;
-  }
-
-  li {
-    padding: 0.5em;
-  }
-
-  li span {
-    display: inline-block;
-  }
-
-  li .resultInner {
-    display: flex;
-    justify-content: space-between;
-  }
-
-  li .dns {
-    display: flex;
-    flex-direction: column;
-    align-items: left;
-  }
-
-  @keyframes fadeIn {
-    from {
-      opacity: 0;
-    }
-    to {
-      opacity: 1;
-    }
-  }
-
-  .fade-in {
-    opacity: 0; /* Start with the items hidden */
-    animation: fadeIn 1s forwards; /* 1s duration, forwards to keep the final state */
-  }
-
-  .highlight-green {
-    background-color: lightgreen;
-    color: black;
-  }
-
-  .highlight-red {
-    background-color: lightcoral;
-  }
-
-  .dnsResult {
-    display: flex;
-    flex-direction: column;
-    align-items: left;
-  }
-</style>
--- vite.config.js
+++ vite.config.js
@@ -6,4 +6,9 @@ import { svelte } from "@sveltejs/vite-plugin-svelte";
 export default defineConfig({
   plugins: [svelte()],
   base: "./", // Set the base option to './' for relative asset paths
+  define: {
+    "process.env.NODE_ENV": JSON.stringify(
+      process.env.NODE_ENV || "development"
+    ),
+  },
 });
